2cd3ac7044501ffc37e148dc3469bbee182b9881
fox2049 <fox@sues.edu.cn>
2021-02-24 08:41:54 +0800

    add telegram

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ async function start() {
     let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
     resp = ` ${res2} ` + ` ${res} `  + new Date().toLocaleDateString() + content
-    await bot.sendMessage(TG_ID, / 京东签到成功 /)
+    await bot.sendMessage(TG_ID, content)
   }
 }
 
